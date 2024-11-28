47194ed3c905ed5ae9a6622847f5350ae0b1867c
DjonatasBorges <interdjonatas@gmail.com>
2024-11-28 08:24:50 -0300

    static

--- src/api.js
+++ src/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 // Criar instância do axios com a URL base da API
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api', // URL base da API
+  baseURL: 'https://eco-tec-api.onrender.com/api', // URL base da API
   headers: {
     'Content-Type': 'application/json', // Defina o formato das requisições
   },
